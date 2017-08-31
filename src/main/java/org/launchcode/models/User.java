0003bc5886cfc4e800c365b4db823b13ae020278
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-08-30 20:36:53 -0700

    added User model with fields

---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,14 @@
+package org.launchcode.models;
+
+public class User {
+
+    /*
+    *   Fields
+    */
+    private String username;
+    private String email;
+    private String password;
+    private int id;
+    private static int nextId = 1000000;
+
+}
