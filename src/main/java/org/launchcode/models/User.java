3d80893f0be96f5c8699afe4d49f501ea76f0e98
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-08-30 20:37:28 -0700

    added getters/setters to User model

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -11,4 +11,24 @@ public class User {
     private int id;
     private static int nextId = 1000000;
 
+    /*
+    *   Getters and Setters
+    */
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public int getId() { return id; }
+    private void setId(int id) { this.id = id; }
+
+    public static int getNextId() { return nextId; }
+    private static void setNextId(int nextId) { User.nextId = nextId; }
+    /*
+    *   Instance Methods
+    */
 }
